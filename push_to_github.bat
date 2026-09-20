8dbeed766c507117855cc9e03aef679dea5f5b37
TheNameless0110 <TheNameless0110@users.noreply.github.com>
2026-09-21 02:46:02 +0530

    Increased profile pic size on mobile and scaled down floating flowers

---
+++ push_to_github.bat
@@ -0,0 +1,4 @@
+git remote set-url origin https://github.com/TheNameless0110/HBD-2026.git 2>nul || git remote add origin https://github.com/TheNameless0110/HBD-2026.git
+git branch -M main
+git push -u origin main
+pause
